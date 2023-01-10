72069fc81443a78015917b464e9bcce5f6d2a46f
Johan Larsson <johan.larsson@ub.gu.se>
2023-01-10 09:17:25 +0100

    simplify localeswitcher

--- frontend/src/components/LocaleToggler.vue
+++ frontend/src/components/LocaleToggler.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="locale-toggler">
-      <button class="btn nav-link" id="navbarLanguageToggle" @click="setLanguage($t('components.the_header.language_otherCode'))">
+      <button class="btn nav-link" id="navbarLanguageToggle" @click="switchLocalePath($t('components.the_header.language_otherCode'))">
         <font-awesome-icon icon="globe" /> {{$t('components.the_header.language_otherName')}}  
       </button>
     </div>
@@ -9,19 +9,7 @@
 </template>
 
 <script>
-import { useI18n } from 'vue-i18n'
-import { useRouter } from 'vue-router'
 export default {
-  setup() {
-    const i18n = useI18n({ useScope: 'global' })
-    const router = useRouter()
-    function setLanguage(locale) {
-      i18n.locale.value = locale
-    }
-    return {
-      setLanguage
-    }
-  }
 }
 </script>
 
