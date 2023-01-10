073ce20f6764dfff250157969b50314bdb569a6f
Johan Larsson <johan.larsson@ub.gu.se>
2023-01-10 11:10:24 +0100

    initial commit for nuxt-3

---
+++ frontend-nuxt-3/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <header>header</header>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ frontend-nuxt-3/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        this is index
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
