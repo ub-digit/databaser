5f556539f0ee63cdbb767cba12a051bd65b37b74
Lars Berglén <lars.berglen@ub.gu.se>
2023-01-11 10:55:41 +0100

    removed payload wrapper in controller, changed to json response

--- backend/lib/databases_web/controllers/databases_controller.ex
+++ backend/lib/databases_web/controllers/databases_controller.ex
@@ -2,32 +2,25 @@ defmodule DatabasesWeb.DatabaseController do
   use DatabasesWeb, :controller
 
   def alert(conn, _params) do
-    text conn, Jason.encode!(%{"en" => "english message", "sv" => "svenskt meddelande"})
+    json conn, %{"en" => "english message", "sv" => "svenskt meddelande"}
   end
 
   def index(conn, params) do
-    payload = case params do
-      %{"payload" => p} -> Jason.decode!(p)
-      _ -> %{}
-    end
-    databases = Databases.Resource.Search.search(payload)
+
+    databases = Databases.Resource.Search.search(params)
     json conn, databases
   end
 
-  def get_popular_databases(conn, params) do
-    {databases, _} = Databases.Resource.Search.popular_databases(get_lang(params))
-    text conn, Jason.encode!(databases)
+  def get_popular_databases(conn, %{"lang" => lang}) do
+    {databases, _} = Databases.Resource.Search.popular_databases(lang)
+    json conn, databases
   end
 
 
 #TODO: payload  contain a "paload" object. Correct this in frontend
-  def show(conn, %{"id" => _, "payload" => payload}) do
-    database = Databases.Resource.Search.show(Jason.decode!((payload)))
+  def show(conn,  payload) do
+    database = Databases.Resource.Search.show(payload)
     json conn, database
   end
 
-  def get_lang (params) do
-    Jason.decode!(Map.get(params, "payload"))
-    |> Map.get("lang")
-  end
 end
