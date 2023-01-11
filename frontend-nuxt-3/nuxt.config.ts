0af713b49246fd0cfa1c6af6e42d9b3b6a204224
Johan Larsson <johan.larsson@ub.gu.se>
2023-01-11 11:05:04 +0100

    nuxt frontend 3 config

--- frontend-nuxt-3/nuxt.config.ts
+++ frontend-nuxt-3/nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
     typescript: {
         shim: false
-    }
+    },
+  modules: [
+    '@nuxtjs/i18n',
+  ],
+  i18n: {
+    /* module options */
+    strategy: 'prefix_except_default',
+    defaultLocale: 'sv'
+  }
 })
