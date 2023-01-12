415cb6576e527b181fecfeb40b381d2d2e821ae6
Johan Larsson <johan.larsson@ub.gu.se>
2023-01-12 09:42:19 +0100

    removed payload

--- frontend-nuxt/store/index.js
+++ frontend-nuxt/store/index.js
@@ -15,7 +15,7 @@ export const actions = {
   async fetchDatabases(context, payload) {
     try {
      // NProgress.start();
-      const result = await this.$axios.get(`/databases`, {params: {payload}});
+      const result = await this.$axios.get(`/databases`, {params: payload});
       return result.data;
     } catch (error) {
       console.log(error.message);
@@ -25,7 +25,7 @@ export const actions = {
   },
   async fetchPopularDatabases(context, payload) {
     try {
-      const result = await this.$axios.get('/databases/popular', {params: {payload}});
+      const result = await this.$axios.get('/databases/popular', {params: payload});
       return result.data;
     } catch (error) {
       console.log(error.message);
@@ -34,7 +34,7 @@ export const actions = {
   async fetchDatabase({context}, payload) {
     try {
      // NProgress.start();
-      const result = await this.$axios.get(`/databases/${payload.id}`, {params: {payload}});
+      const result = await this.$axios.get(`/databases/${payload.id}`, {params: payload});
       return result.data;
     } catch (error) {
       console.log(error.message);
