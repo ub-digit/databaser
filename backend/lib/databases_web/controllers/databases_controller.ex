9f10f236d68b9ea2a24535510bf29871949890b5
Lars Berglén <lars.berglen@ub.gu.se>
2023-01-13 12:02:37 +0100

    typed parameters plug

--- backend/lib/databases_web/controllers/databases_controller.ex
+++ backend/lib/databases_web/controllers/databases_controller.ex
@@ -6,26 +6,10 @@ defmodule DatabasesWeb.DatabaseController do
   end
 
   def index(conn, params) do
-
-    IO.inspect(params, label: "PARAMS")
-
-    topic = get_topic(params)
-    params = Map.put(params, "topic", topic)
-    params = get_show_free(params)
     databases = Databases.Resource.Search.search(params)
     json conn, databases
   end
 
-  def get_topic(params) when not is_map_key(params, "topic"), do: nil
-  def get_topic(%{"topic" => nil}), do: nil
-  def get_topic(%{"topic" => topic}) do
-    Integer.parse(topic)
-    |> elem(0)
-  end
-  def get_show_free(params) when not is_map_key(params, "show_free"), do: params
-  def get_show_free(%{"show_free" => "true"} = params), do: Map.put(params, "show_free", true)
-  def get_show_free(%{"show_free" => "false"} = params), do: Map.put(params, "show_free", false)
-  def get_show_free(%{"show_free" => ""} = params), do: Map.delete(params, "show_free")
 
   def get_popular_databases(conn, %{"lang" => lang}) do
     {databases, _} = Databases.Resource.Search.popular_databases(lang)
