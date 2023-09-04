2fa781f4e5982907089c456c8352189e9e52a923
Johan Larsson <karmajohan@gmail.com>
2023-09-04 11:04:22 +0200

    prettier formatting

--- frontend-admin/src/main.js
+++ frontend-admin/src/main.js
@@ -1,41 +1,52 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
-import router from "./router";
-import axios from 'axios';
-import { plugin, defaultConfig } from '@formkit/vue';
-import {useFontawesome} from '@/plugins/fontawesome.js';
-import Toast from 'vue-toastification';
+import axios from "axios";
+import { plugin, defaultConfig } from "@formkit/vue";
+import { useFontawesome } from "@/plugins/fontawesome.js";
+import Toast from "vue-toastification";
 import "@/scss/custom.scss";
-import 'nprogress/nprogress.css';
+import "nprogress/nprogress.css";
 import { i18n } from "./locales";
-import auth from '@/plugins/auth.js'
-import http from '@/plugins/http.js'
+import auth from "@/plugins/auth.js";
+import http from "@/plugins/http.js";
+import router from "@/router/index.js";
 
-const local_url = '/cgi-bin/backend.cgi'
-const {FontAwesomeIcon} = useFontawesome();
+const local_url = "/cgi-bin/backend.cgi";
+const { FontAwesomeIcon } = useFontawesome();
 
-axios.get(local_url).then((result) => {
-  return result.data
-},() => {
-  return {
-    backend_url: /* process.env["VUE_APP_API_BASE_URL"] ||*/ "http://localhost:4010",
-  }
-}).then((data) => {
-  let app = createApp(App)
-  app.baseURL = data.backend_url
-  app
-  .provide('baseURL', data.backend_url)
-  //.provide('alertURL', data.alert_url)
-  .component("font-awesome-icon", FontAwesomeIcon)
-  .use(router)
-  .use(createPinia())
-  .use(i18n)
-  .use(http)
-  .use(auth)
-  .use(plugin, defaultConfig)
-  .use(Toast, { timeout: 3000, position: 'top-center', transition: 'Vue-Toastification__fade', hideProgressBar: true })
+axios
+  .get(local_url)
+  .then(
+    (result) => {
+      return result.data;
+    },
+    () => {
+      return {
+        backend_url:
+          /* process.env["VUE_APP_API_BASE_URL"] ||*/ "http://127.0.0.1:4010",
+      };
+    }
+  )
+  .then((data) => {
+    let app = createApp(App);
+    app.baseURL = data.backend_url;
+    app
+      .provide("baseURL", data.backend_url)
+      //.provide('alertURL', data.alert_url)
+      .component("font-awesome-icon", FontAwesomeIcon)
+      .use(router)
+      .use(createPinia())
+      .use(i18n)
+      .use(http)
+      .use(auth)
+      .use(plugin, defaultConfig)
+      .use(Toast, {
+        timeout: 3000,
+        position: "top-center",
+        transition: "Vue-Toastification__fade",
+        hideProgressBar: true,
+      })
 
-  .mount("#app");
-})
-
+      .mount("#app");
+  });
