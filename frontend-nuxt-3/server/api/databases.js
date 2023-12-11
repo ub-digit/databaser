26b2c15e68fe5e0403427aaf4b3768a3e4e286a3
Johan Larsson <johan.larsson@ub.gu.se>
2023-12-11 11:47:11 +0100

    adds try catch

--- frontend-nuxt-3/server/api/databases.js
+++ frontend-nuxt-3/server/api/databases.js
@@ -1,8 +1,13 @@
 export default defineEventHandler(async (event) => {
-    const config = useRuntimeConfig();
-    const query = getQuery(event);
-    const res = await $fetch(config.API_BASE_URL + 'databases/', {
-        params: query
-    })
+  const config = useRuntimeConfig();
+  const query = getQuery(event);
+
+  try {
+    const res = await $fetch(config.API_BASE_URL + "databases/", {
+      params: query,
+    });
     return res;
-})
+  } catch (error) {
+    console.error(error);
+  }
+});
