657051e9b8f25a5a8b058474ae8e73f9c522d426
Johan Larsson <johan.larsson@ub.gu.se>
2023-12-11 11:37:12 +0100

    new bg-color

--- frontend-nuxt-3/components/Alert.vue
+++ frontend-nuxt-3/components/Alert.vue
@@ -1,38 +1,39 @@
 <template>
-    <div class="alert alert-warning" v-if="data && data.show" >
-      <div class="row">
-        <div class="col-auto">
-          <font-awesome-icon icon="circle-info" />
-        </div>
-        <div class="col">
-          <span v-html="message"></span>
-        </div>
+  <div class="alert alert-warning" v-if="data && data.show">
+    <div class="row">
+      <div class="col-auto">
+        <font-awesome-icon icon="circle-info" />
+      </div>
+      <div class="col">
+        <span v-html="message"></span>
       </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-import {onUnmounted } from 'vue'
-const {t, getLocale} = useI18n();
-const props = defineProps(['url', 'interval'])
-const pollInterval =    (props.interval ? props.interval * 1000 : 60000)
+import { onUnmounted } from "vue";
+const { t, getLocale } = useI18n();
+const props = defineProps(["url", "interval"]);
+const pollInterval = props.interval ? props.interval * 1000 : 60000;
 let pollingID = null;
-const { data, pending, error, refresh } = await useFetch(props.url)
-
+const { data, pending, error, refresh } = await useFetch(props.url);
 
 const message = computed(() => {
-    return data.value[getLocale()]
-})
+  return data.value[getLocale()];
+});
 
 pollingID = setInterval(() => {
-        refresh();
-    }, pollInterval);
+  refresh();
+}, pollInterval);
 
-
-onUnmounted(() => clearInterval(pollingID))
-
+onUnmounted(() => clearInterval(pollingID));
 </script>
 
 <style lang="scss" scoped>
-
+.alert-warning {
+  background-color: #fff3cd;
+  border-color: #ffeeba;
+  color: #856404;
+}
 </style>
