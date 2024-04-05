5143ee1b9a0d71c83a129d051e5ab7b7e46201f1
Johan Larsson <johan.larsson@ub.gu.se>
2024-04-05 10:14:58 +0200

    alt names for mediatypes

--- frontend-admin/src/components/MediatypeForm.vue
+++ frontend-admin/src/components/MediatypeForm.vue
@@ -2,43 +2,58 @@
   <div class="edit-topic-wrapper">
     <div v-if="mediatype_initial_state">
       <FormKit type="form" :actions="false">
-          <div class="row">
-            <div class="col">
-              <h1>{{props.title}}</h1>
-            </div>
-            <div class="col-auto align-self-end">
-                <router-link v-if="mediatype_initial_state.id" class="btn btn-light me-2" :to="{name: 'MediatypeShow', params: {id: mediatype_initial_state.id}}">Cancel</router-link>
-              <FormKit
-                :classes="{
-                  outer: 'd-inline-block',
-                  input: 'btn btn-primary'
-                }"
-                @click.prevent="saveMediatype"
-                type="submit"
-                label="Save"
-                :disabled="!isDirty"
-              />
-            </div>
+        <div class="row">
+          <div class="col">
+            <h1>{{ props.title }}</h1>
+          </div>
+          <div class="col-auto align-self-end">
+            <router-link
+              v-if="mediatype_initial_state.id"
+              class="btn btn-light me-2"
+              :to="{
+                name: 'MediatypeShow',
+                params: { id: mediatype_initial_state.id },
+              }"
+              >Cancel</router-link
+            >
+            <FormKit
+              :classes="{
+                outer: 'd-inline-block',
+                input: 'btn btn-primary',
+              }"
+              @click.prevent="saveMediatype"
+              type="submit"
+              label="Save"
+              :disabled="!isDirty"
+            />
+          </div>
         </div>
 
         <div v-if="props.errors" class="row mt-3">
           <div class="col">
-            <div v-if="props.errors.media_type" class="alert alert-danger" role="alert">
+            <div
+              v-if="props.errors.media_type"
+              class="alert alert-danger"
+              role="alert"
+            >
               <ul>
-                <li v-for="(error,index) in props.errors.media_type" :key="index">
-                  {{ $t('mediatype.error_codes.' + error.field) }}: {{ $t('mediatype.error_codes.' + error.error_code) }} 
+                <li
+                  v-for="(error, index) in props.errors.media_type"
+                  :key="index"
+                >
+                  {{ $t("mediatype.error_codes." + error.field) }}:
+                  {{ $t("mediatype.error_codes." + error.error_code) }}
                 </li>
               </ul>
+            </div>
           </div>
-        </div>
-          
         </div>
         <div class="row">
           <div class="mb-3 col-6">
-            <FormKit 
-              type="text" 
+            <FormKit
+              type="text"
               :classes="{
-                input: 'form-control'
+                input: 'form-control',
               }"
               label="Mediatype (en)"
               name="mediatype_initial_state.name_en"
@@ -48,15 +63,41 @@
             />
           </div>
           <div class="mb-3 col-6">
-            <FormKit 
-              type="text" 
+            <FormKit
+              type="text"
               :classes="{
-                input: 'form-control'
+                input: 'form-control',
               }"
               label="Mediatype (sv)"
               name="mediatype_initial_state.name_sv"
               v-model="mediatype_initial_state.name_sv"
               placeholder="Enter mediatype in swedish"
+            />
+          </div>
+        </div>
+        <div class="row">
+          <div class="mb-3 col-6">
+            <FormKit
+              type="text"
+              :classes="{
+                input: 'form-control',
+              }"
+              label="Mediatype alternative names (en)"
+              name="mediatype_initial_state.alternative_names_en"
+              v-model="mediatype_initial_state.alternative_names_en"
+              placeholder="Alternative names (comma separated list)"
+            />
+          </div>
+          <div class="mb-3 col-6">
+            <FormKit
+              type="text"
+              :classes="{
+                input: 'form-control',
+              }"
+              label="Mediatype (sv)"
+              name="mediatype_initial_state.alternative_names_sv"
+              v-model="mediatype_initial_state.alternative_names_sv"
+              placeholder="Alternative names (comma separated list)"
             />
           </div>
         </div>
@@ -66,63 +107,66 @@
 </template>
 
 <script>
-import { useRoute, useRouter, onBeforeRouteLeave } from 'vue-router'
-import { computed, ref } from 'vue'
-import {useMediatypesStore} from '@/stores/mediatypes'
-import _ from 'lodash'
+import { useRoute, useRouter, onBeforeRouteLeave } from "vue-router";
+import { computed, ref } from "vue";
+import { useMediatypesStore } from "@/stores/mediatypes";
+import _ from "lodash";
 
 export default {
-  name: 'MediatypeForm',
-  emits: ['saveMediatype'],
-  props: ['mediatype', 'title', 'errors'],
+  name: "MediatypeForm",
+  emits: ["saveMediatype"],
+  props: ["mediatype", "title", "errors"],
   setup(props, ctx) {
     const router = useRouter();
     const route = useRoute();
     const store = useMediatypesStore();
     const mediatype = props.mediatype;
     const mediatype_initial_state = ref(_.cloneDeep(mediatype));
-    const isDirty = computed(() => _.isEqual(mediatype, mediatype_initial_state.value) ? false: true);
+    const isDirty = computed(() =>
+      _.isEqual(mediatype, mediatype_initial_state.value) ? false : true
+    );
     let isSaved = false;
-  
 
     onBeforeRouteLeave(() => {
       if (isDirty.value && !isSaved) {
-        const answer = window.confirm("It looks like you have been editing something. If you leave before saving, your changes will be lost.");
+        const answer = window.confirm(
+          "It looks like you have been editing something. If you leave before saving, your changes will be lost."
+        );
         if (!answer) return false;
       }
-    })
+    });
     const saveMediatype = () => {
-      ctx.emit('saveMediatype', mediatype_initial_state.value);
+      ctx.emit("saveMediatype", mediatype_initial_state.value);
       isSaved = true;
-    }
+    };
     return {
       mediatype_initial_state,
       saveMediatype,
       isDirty,
-      props
-    }
-  }
-}
+      props,
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style  lang="scss">
-  .edit-topic-wrapper {
-    .formkit-label {
-      font-weight: bold;
-    }
-    #sub-topics-wrapper {
-      margin-left: 20px;
-      #sub-topics-list {
-        li  {
-          padding: 20px;
-          margin-bottom: 20px;
-          border: 1px dotted #ccc;
-          &.is_marked_for_removal {
-            opacity: .5;
-          }
+<style lang="scss">
+.edit-topic-wrapper {
+  .formkit-label {
+    font-weight: bold;
+  }
+  #sub-topics-wrapper {
+    margin-left: 20px;
+    #sub-topics-list {
+      li {
+        padding: 20px;
+        margin-bottom: 20px;
+        border: 1px dotted #ccc;
+        &.is_marked_for_removal {
+          opacity: 0.5;
         }
       }
     }
   }
+}
 </style>
