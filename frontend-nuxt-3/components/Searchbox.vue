a171ddceab81b7df51837a2c7f63f8bd4854a9d5
Johan Larsson <johan.larsson@ub.gu.se>
2024-11-25 13:30:07 +0100

    fix arialabel

--- frontend-nuxt-3/components/Searchbox.vue
+++ frontend-nuxt-3/components/Searchbox.vue
@@ -14,12 +14,14 @@
           class="form-control form-control-lg"
           :placeholder="t('components.search_bar.placeholder')"
           v-model="localSearchStr"
+          :aria-label="t('components.search_bar.input_aria_label')"
         />
         <input
           class="btn btn-primary"
           type="submit"
           @click.prevent="submitSearch(localSearchStr)"
           :value="t('components.search_bar.submit')"
+          :aria-label="t('components.search_bar.submit')"
         />
       </form>
       <div
