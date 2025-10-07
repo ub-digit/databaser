ca2d3fa711cca0f1fb64910c04d663c285a76dbc
Johan Larsson <johan.larsson@ub.gu.se>
2025-10-07 11:29:10 +0200

    new en translations for accessibility

--- frontend-admin/src/locales/en.js
+++ frontend-admin/src/locales/en.js
@@ -1,52 +1,53 @@
-
-export const en = { 
-    application_title: 'Databases - admin',
-    access_information_codes: {
-        available_to_the_university_of_gothenburg: 'Available to the University of Gothenburg',
-        freely_available: 'Freely available',
-        available_to_the_university_of_gothenburg_on_campus_only_available_to_anyone: 'Available to the University of Gothenburg, on campus only. Available to anyone using the libraries computers',
-        available_to_the_university_of_gothenburg_available_to_anyone_using_the_libraries_computers: 'Available to the University of Gothenburg. Available to anyone using the libraries computers'
+export const en = {
+  application_title: "Databases - admin",
+  access_information_codes: {
+    available_to_the_university_of_gothenburg:
+      "Accessible to students and employees of the University of Gothenburg.",
+    freely_available: "Freely available",
+    available_to_the_university_of_gothenburg_on_campus_only_available_to_anyone:
+      "Accessible to students and employees at the University of Gothenburg on campus and to employees via a secure connection (VPN) Accessible to anyone using the libraries’ computers.",
+    available_to_the_university_of_gothenburg_available_to_anyone_using_the_libraries_computers:
+      "Accessible to students and employees at the University of Gothenburg. Accessible to anyone using the libraries’ computers.",
+  },
+  terms_of_use_code: {
+    print_article_chapter: "Print article or chapter",
+    download_article_chapter: "Download article or chapter",
+    course_pack_print: "Print course pack",
+    gul_course_pack_electronic:
+      "Upload course pack to learning management system",
+    scholarly_sharing: "Scholarly sharing",
+    interlibrary_loan: "Interlibrary loan",
+    ai_rules: "AI",
+  },
+  topic: {
+    error_codes: {
+      name_en: "Topic name (en)",
+      name_sv: "Topic name (sv)",
+      validation_required: "Field is required",
+      constraint_unique: "Unique required",
     },
-    terms_of_use_code: {
-        print_article_chapter: "Print article or chapter",
-        download_article_chapter: "Download article or chapter",
-        course_pack_print: "Print course pack",
-        gul_course_pack_electronic: "Upload course pack to learning management system",
-        scholarly_sharing: "Scholarly sharing",
-        interlibrary_loan: "Interlibrary loan",
-        ai_rules: "AI"
+  },
+  sub_topics: {
+    error_codes: {
+      name_en: "Subtopic name (en)",
+      name_sv: "Subtopic name (sv)",
+      validation_required: "Field is required",
+      constraint_unique: "Unique required",
     },
-    topic: {
-        error_codes: {
-            name_en: 'Topic name (en)',
-            name_sv: 'Topic name (sv)',
-            validation_required: 'Field is required',
-            constraint_unique: 'Unique required'
-            
-        }
+  },
+  publisher: {
+    error_codes: {
+      name: "Publisher name",
+      validation_required: "Field is required",
+      constraint_unique: "Unique required",
     },
-    sub_topics: {
-        error_codes: {
-            name_en: 'Subtopic name (en)',
-            name_sv: 'Subtopic name (sv)',
-            validation_required: 'Field is required',
-            constraint_unique: 'Unique required'
-        }
-
+  },
+  mediatype: {
+    error_codes: {
+      name_en: "Mediatype name (en)",
+      name_sv: "Meditype name (sv)",
+      validation_required: "Field is required",
+      constraint_unique: "Unique required",
     },
-    publisher: {
-        error_codes: {
-            name: 'Publisher name',
-            validation_required: 'Field is required',
-            constraint_unique: 'Unique required'
-        }
-    },
-    mediatype: {
-        error_codes: {
-            name_en: 'Mediatype name (en)',
-            name_sv: 'Meditype name (sv)',
-            validation_required: 'Field is required',
-            constraint_unique: 'Unique required'
-        }
-    }
+  },
 };
